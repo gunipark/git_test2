c35e8e3ba774dc722b314e6482624b76bd59a5db
Park EunSol <eunsolguni1353@gmail.com>
2024-09-24 14:38:15 +0900

    변경

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,11 +1,10 @@
 package git_test2;
 
 public class Hello {
-        String message = "안녕하세요";
+     String message = "안녕하세요";
 
     public void say() {
         System.out.println(message);
     }
-
 
 }
